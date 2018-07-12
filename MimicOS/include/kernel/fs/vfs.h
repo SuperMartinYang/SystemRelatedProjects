d8d5c2f2e55436248a32c485b9d58c590d5d1cf3
SuperMartinYang <31331276+SuperMartinYang@users.noreply.github.com>
2018-07-12 14:30:35 -0400

    file system headers added

---
+++ MimicOS/include/kernel/fs/vfs.h
@@ -0,0 +1,111 @@
+#ifndef _KERNEL_FS_VFS_H
+#define _KERNEL_FS_VFS_H
+
+#include <sys/types.h>
+
+#define VFS_MAXFILENAME 	256
+
+#define VFS_FILE 			0
+#define VFS_DIRECTORY		1
+#define VFS_DEVICE			2
+
+#define VFS_SEEK_START		0
+#define VFS_SEEK_CURRENT	1
+#define VFS_SEEK_END		2
+
+#define VFS_MODE_READ		1
+#define VFS_MODE_WRITE		2
+#define VFS_MODE_READWRITE	(VFS_MODE_READ | VFS_MODE_WRITE)
+#define VFS_MODE_CREATE		4
+#define VFS_MODE_TRUNCATE	8
+#define VFS_MODE_APPEND		9
+
+struct VFS_HANDLE
+{
+	struct VFS_MOUNTPOINT * mount;
+	void * data_ptr;
+	int mode;
+};
+
+typedef int (*rw)(struct VFS_HANDLE *, BYTE *, DWORD);
+
+struct VFS_FILESYSTEM_CALLTABLE
+{
+	struct VFS_HANDLE * (*open)(struct VFS_HANDLE *, char *);
+	int (*close)(struct VFS_HANDLE *);
+	int (*clone)(struct VFS_HANDLE *, struct VFS_HANDLE *);
+	rw read;
+	rw write;
+	int (*seek)(struct VFS_HANDLE *, DWORD, BYTE);
+	int (*control)(struct VFS_HANDLE *, DWORD, DWORD);
+	int (*create)(struct VFS_MOUNTPOINT *, char *);
+	int (*delete)(struct VFS_MOUNTPOINT *, char *);
+	int (*rename)(struct VFS_MOUNTPOINT *, char *, char *);
+	int (*copy)(struct VFS_MOUNTPOINT *, char *, char *);
+	struct VFS_DIRLIST_ENTRY * (*list)(struct VFS_MOUNTPOINT *, char *);
+	void * (*mount)(char *, char *, int);
+	int (*unmount)(struct VFS_MOUNTPOINT *, char *);
+};
+
+struct VFS_FILESYSTEM
+{
+	int fstype;
+	struct VFS_FILESYSTEM_CALLTABLE calltable;
+	struct VFS_FILESYSTEM * prev;
+};
+
+struct VFS_MOUNTPOINT
+{
+	struct VFS_FILESYSTEM * fs;
+	void * data_ptr;
+	char * mountpoint;
+	char * device;
+	struct VFS_MOUNTPOINT * next;
+};
+
+#define VFS_NAMESIZE	32
+
+struct VFS_DIRLIST_ENTRY
+{
+	char name[VFS_NAMESIZE];
+	int attributes;
+	int size;
+};
+
+int vfs_init(void);
+
+int vfs_register(struct VFS_FILESYSTEM *);
+
+int vfs_unregister(int);
+
+// volume operations
+int vfs_mount(char *, char *, int);
+
+int vfs_unmount(char *);
+
+// file operations
+struct VFS_HANDLE * vfs_open(char *, int);
+
+int vfs_close(struct VFS_HANDLE *);
+
+struct VFS_HANDLE * vfs_clone(struct VFS_HANDLE *);
+
+int vfs_read(struct VFS_HANDLE *, BYTE *, DWORD);
+
+int vfs_write(struct VFS_HANDLE *, BYTE *, DWORD);
+
+int vfs_seek(struct VFS_HANDLE *, DWORD, BYTE);
+
+int vfs_control(struct VFS_HANDLE *, DWORD, DWORD);
+
+int vfs_create(char *);
+
+int vfs_delete(char *);
+
+int vfs_rename(char *);
+
+int vfs_copy(char *, char *);
+
+struct VFS_DIRLIST_ENTRY * vfs_list(char *);
+
+#endif
---
+++ MimicOS/include/sys/types.h
@@ -0,0 +1,32 @@
+#ifndef _SYS_TYPES_H_
+#define _SYS_TYPES_H_
+
+#define TRUE	0X01
+#define FALSE	0X00
+
+#define SUCCESS	0
+#define FAIL	-1
+
+#define ASM		__asm__ __volatile__
+#define PACKED	__attribute__( (packed) )
+
+#define NULL	((void *) 0x00000000)
+
+typedef int				BOOL;
+typedef unsigned char 	BYTE;
+typedef unsigned short	WORD;
+typedef unsigned long	DWORD;
+
+#define SIZE_BYTE	1
+#define SIZE_WORD	2
+#define SIZE_DWORD	4
+
+typedef char *	va_list;
+
+#define __va_rounded_size(TYPE) 	(((sizeof(TYPE) + sizeof(int) - 1) / sizeof(int) * sizeof(int)))
+
+#define va_start(AP, LASTAGE) 		(AP = ((char *) &(LASTAGE) + __va_rounded_size(LASTAGE)))
+
+#define va_arg(AP, TYPE) 			(AP += __va_rounded_size(TYPE), *((TYPE *) (AP - __va_rounded_size(TYPE))))
+
+#endif
