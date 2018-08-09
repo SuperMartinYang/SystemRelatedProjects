b2d693319c8f734052ee2e38c5d16f52f9b47d36
supermartinyang <shy58@pitt.edu>
2018-08-09 17:02:31 -0400

    complete version

--- MimicOS/src/kernel/fs/vfs.c
+++ MimicOS/src/kernel/fs/vfs.c
@@ -128,49 +128,6 @@ struct VFS_MOUNTPOINT * vfs_file2mountpoint(char * filename){
 	return mount;
 }
 
-<<<<<<< HEAD
-// open the file, actually get the VFS_HANDLE for other usage
-struct VFS_HANDLE * vfs_open(char * filename, int mode){
-	struct VFS_HANDLE * handle;
-	struct VFS_MOUNTPOINT * mount;
-	char name[VFS_MAXFILENAME], * name_ptr;
-	// copy the name
-	name_ptr = (char *)&name;
-	strcpy(name_ptr, filename);
-	// find the mountpoint for that file
-	mount = vfs_file2mountpoint(name_ptr);
-	if (mount == NULL)
-		return NULL;
-	// advance the filename past the mountpoint
-	name_ptr = (char *)(name_ptr + strlen(mount->mountpoint));
-	// call the file system driver to open
-	if (mount->fs->calltable.open == NULL)
-		return NULL;
-	handle = (struct VFS_HANDLE *)mm_kmalloc(sizeof(struct VFS_HANDLE));
-	handle->mount = mount;
-	handle->mode = mode;
-	//  try to open the file on the mounted file system, if open successfully, handle->data_ptr will be filled
-	if (mount->fs->calltable.open(handle, name_ptr) != NULL){
-		// set the file position to the end of the file if in append mode
-		if ((handle->mode & VFS_MODE_APPEND) == VFS_MODE_APPEND)
-			vfs_seek(handle, 0, VFS_SEEK_END);
-		return handle;
-	}else {
-		// if we fail to open the file but are in create mode, we can create the file
-		// 	TODO: test the failed open() result for a value like FILE_NOT_FOUND
-		//	otherwise we could end up in an infinite recursive loop
-		if ((handle->mode & VFS_MODE_CREATE) == VFS_MODE_CREATE){
-			if (mount->fs->calltable.create != NULL){
-				// try to create
-				name_ptr = (char *)&name;
-				strcpy(name_ptr, filename);
-				name_ptr = (char *)(name_ptr + strlen(mount->mountpoint));
-
-				if (mount->fs->calltable.create(mount, name_ptr) != FAIL){
-					if (mount->fs->calltable.open(handle, name_ptr) != NULL){
-						if ((handle->mode & VFS_MODE_APPEND) == VFS_MODE_APPEND)
-							vfs_seek(handle, 0, VFS_SEEK_END);
-=======
 struct VFS_HANDLE * vfs_open( char * filename, int mode ){
 	struct VFS_HANDLE * handle;
 	struct VFS_MOUNTPOINT * mount;
@@ -212,84 +169,10 @@ struct VFS_HANDLE * vfs_open( char * filename, int mode ){
 					if( mount->fs->calltable.open( handle, name_ptr ) != NULL ){
 						if( (handle->mode & VFS_MODE_APPEND) == VFS_MODE_APPEND )
 							vfs_seek( handle, 0, VFS_SEEK_END );
->>>>>>> a0e15c3e72c1de9d9796f932ac34596792dae26e
 						return handle;
 					}
 				}
 			}
-<<<<<<< HEAD
-		}
-	}
-	// free handle
-	mm_kfree(handle);
-	return NULL;
-}
-
-int vfs_close(struct VFS_HANDLE * handle){
-	// close and free handle
-	if (handle == NULL) return FAIL;
-	if (handle->mount->fs->calltable.close != NULL){
-		int ret;
-		ret = handle->mount->fs->calltable.close(handle);
-		mm_kfree(handle);
-		return ret;
-	}
-	return FAIL;
-}
-
-// clone the file handle, which clone all the file content, todo
-struct VFS_HANDLE * vfs_clone(struct VFS_HANDLE * handle){
-	struct VFS_HANDLE *  clone;
-	if (handle == NULL)
-		return NULL;
-	if (handle->mount->fs->calltable.clone != NULL){
-		clone = (struct VFS_HANDLE *)mm_kmalloc(sizeof(VFS_HANDLE));
-		clone->mount = handle.mount;
-		clone->mode = handle->mode;
-		if (handle->mount->fs->calltable.clone(handle, clone) == FAIL){
-			mm_kfree(clone);
-			return FAIL;
-		}
-		return clone;
-	}
-	return NULL;
-}
-
-int vfs_read(struct VFS_HANDLE * handle, BYTE * buffer, DWORD size){
-	if (handle == NULL)
-		return FAIL;
-	// test if the file has been opened in the read mode first
-	if ((handle->mode & VFS_MODE_READ) == VFS_MODE_READ)
-		return FAIL;
-	// read the file content
-	if (handle.mount->fs->calltable.read == NULL)
-		return FAIL;
-	return handle.mount->fs->calltable.read(handle, buffer, size);
-}
-
-int vfs_write(struct VFS_HANDLE * handle, BYTE * buffer, DWORD size){
-	int ret;
-	if (handle == NULL)
-		return FAIL;
-	// already in write?
-	if ((handle->mode & VFS_MODE_WRITE) == VFS_MODE_WRITE)
-		return FAIL;
-	// write
-	if (handle->mount->fs->calltable.write == NULL)
-		return FAIL;
-	ret = handle->mount->fs->calltable.write(handle, buffer, size);
-	if (ret != FAIL){
-		// set the file position to the end of the file
-		if (handle->mount->fs->calltable.seek != NULL)
-			vfs_seek(handle, 0, VFS_SEEK_END);
-		return ret;
-	}
-	return FAIL;
-}
-
-int vfs_seek(struct VFS_HANDLE * handle, DWORD offset, BYTE origin){
-	
-=======
 		}		
 	}
 	// if we fail, free the handle and return NULL
@@ -488,5 +371,4 @@ int vfs_init(void){
 	// initialize FAT FILE SYSTEM
 	fat_init();
 	return SUCCESS;
->>>>>>> a0e15c3e72c1de9d9796f932ac34596792dae26e
 }
